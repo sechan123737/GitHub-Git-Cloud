bf225a250dc94ebadc1f54bd96b4a9a98769904d
sechan123737 <sechanmaydiputra27@gmail.com>
2026-09-11 09:46:13 +0700

    Progress Belajar

---
+++ Numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+gambar = np.array([
+    [0, 100, 255],
+    [50, 150, 200],
+    [30, 180, 220]
+])
+
+print(gambar)
+print(gambar.shape)
+print(gambar[2][2])
--- Variable.py
+++ Variable.py
@@ -1,2 +1,15 @@
+nama = "Sechan" #String
+umur = 20 #Integer
+tinggi = 170.5 #Float
+mahasiswa = True #Boolean
+
+print(type(nama))
+print(type(umur))
+print(type(tinggi))
+print(type(mahasiswa))
+
 nama = "Sechan"
-umur = 21
+umur = 20
+
+print(nama)
+print(umur)
